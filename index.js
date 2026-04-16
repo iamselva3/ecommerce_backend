c440f1961f482ff656efc92513c5ea47bf1eb7f6
Selva <imselva1512@gmail.com>
2026-04-16 17:09:35 +0530

    trust proxy added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ import AiRoutes from './routes/AiRoutes.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cookieParser());
 const allowedOrigins = [
